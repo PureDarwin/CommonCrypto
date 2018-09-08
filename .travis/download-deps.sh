091f3939adc67b4c2823f7c4e091a98b0412ce85
William Kent <wjk@sunsol.me>
2018-09-08 14:04:16 -0400

    Add README and Travis CI support

---
+++ .travis/download-deps.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+MY_DIR=$(cd `dirname $0` && pwd)
+SRCROOT=$(dirname $MY_DIR)
+cd $SRCROOT
+
+mkdir -p .dependencies
+cd .dependencies
+
+curl -fsO https://github.com/PureDarwin/xnubuild/releases/download/10.13/xnubuild.hdrs.tar.gz
+tar xf xnubuild.hdrs.tar.gz
+rm xnubuild.hrs.tar.gz
+
+cd $SRCROOT
+cp .travis/travis.xcconfig darwinbuild.xcconfig
---
+++ README.md
@@ -0,0 +1,3 @@
+# CommonCrypto [![Build Status](https://travis-ci.org/PureDarwin/CommonCrypto.svg?branch=master)](https://travis-ci.org/PureDarwin/CommonCrypto)
+
+This repository contains an implementation of PureDarwin's CommonCrypto library, which contains low-level shared cryptographic implementations.
